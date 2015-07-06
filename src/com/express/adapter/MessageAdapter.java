1a3b52e606f5cd7654d080b7e1055b39a0cf87b8
kanfRUI <1175872227@qq.com>
2015-07-06 20:26:04 +0800

    11

--- src/com/express/adapter/MessageAdapter.java
+++ src/com/express/adapter/MessageAdapter.java
@@ -14,7 +14,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class MessageAdapter extends BaseAdapter {
-
+//hbhgyuhyu
 	List<Message> mess;
 	Context context;
 	LayoutInflater inflater;
